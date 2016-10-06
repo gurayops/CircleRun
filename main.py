52f8756ee94d30382dbabd836d50bce2f4dae5d6
gurayyildirim <guray@gurayyildirim.com.tr>
2016-10-06 19:42:36 +0300

    Turns according to button press. Screen scaled

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # -*-coding:utf-8-*-
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.atlas import Atlas
+from kivy.clock import Clock
 
 
 class CircleRun(App):
@@ -20,16 +21,27 @@ class CircleRun(App):
         return appWindow
 
 
-class GameWindow(RelativeLayout):
+class GameWindow(BoxLayout):
     """docstring for GameWindow"""
 
     def __init__(self):
         super(GameWindow, self).__init__()
+        # Determine the level of the game
 
     def atlasTest(self):
         atlas = Atlas('images/centerMachine.atlas')
         print atlas.textures.keys()
 
+    def startMove(self):
+        print "action started."
+        self.diff = -1 if self.ids.gamer.direction == 1 else 1
+        self.ids.gamer.direction = self.diff
+        Clock.unschedule(self.moveUser)
+        Clock.schedule_interval(self.moveUser, 1.0 / 60)
+
+    def moveUser(self, dt):
+        self.ids.gamer.startLocation = (
+            self.ids.gamer.startLocation + self.diff * self.level) % 360
 
 if __name__ == '__main__':
     CircleRun().run()
